d8cc2d297e381ad2f8e6506b2191dc3c98248a49
egecan <egecan18k@gmail.com>
2020-01-13 11:29:50 -0500

    son

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Anasayfa';
 import Portfolio from './components/Portfolio';
+import Bg from './bg.svg';
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
 				<div className="App">
+					<img src={Bg} alt="Bg" className="bgResim" />
 					<Navbar />
 					<Switch>
 						<Route exact path="/" component={Home} />
